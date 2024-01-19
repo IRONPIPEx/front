0fb309aad9a582087caead6af62d8ab7815e1945
김진우 <jinu9707@corealism.com>
2024-01-19 14:40:03 +0900

    test_spring_boot_link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState, useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const QuizList  = () => {
+    const [quizzes, setQuizzes] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost/api/quizzes');
+                if (response.ok) {
+                    const jsonData = await response.json();
+                    setQuizzes(jsonData._embedded.quizzes);
+                } else {
+                    console.error('Failed to fetch');
+                }
+            } catch (error) {
+                console.error('Failed during fetch', error);
+            }
+        };
+
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+            <h1>Quiz List</h1>
+            <ul>
+                {quizzes.map((quiz) => (
+                    <li>
+                        <strong>{quiz.question}</strong> - {quiz._links.self.href}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default QuizList;
